5750b3ae5ab5cf8402a607c5de2b476eb1b3aba3
Dmitry <mr.dimenter@gmail.com>
2021-03-31 02:57:41 +0400

    Create icon for application

--- src/main/java/FX/Main.java
+++ src/main/java/FX/Main.java
@@ -4,13 +4,21 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+import java.io.InputStream;
 
 public class Main extends Application {
 
 
     @Override
     public void start(Stage stage) throws Exception {
+        stage.setTitle("Парсер Яндекс Маркет");
+        InputStream iconStream = getClass().getResourceAsStream("/icons/icon.png");
+        Image image = new Image(iconStream);
+        stage.getIcons().add(image);
+
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/main.fxml"));
         Scene scene = new Scene(root);
         scene.getStylesheets().add("/style/darkstyle.css");
